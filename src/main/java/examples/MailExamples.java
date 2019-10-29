bb5faa399f7305bc3ee8e3be33c650e44327dd12
Lin Gao <aoingl@gmail.com>
2019-10-29 14:58:16 +0800

    Updated docs and examples

--- src/main/java/examples/MailExamples.java
+++ src/main/java/examples/MailExamples.java
@@ -19,11 +19,8 @@ package examples;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.docgen.Source;
-import io.vertx.ext.mail.MailAttachment;
-import io.vertx.ext.mail.MailClient;
-import io.vertx.ext.mail.MailConfig;
-import io.vertx.ext.mail.MailMessage;
-import io.vertx.ext.mail.StartTLSOptions;
+import io.vertx.ext.auth.PubSecKeyOptions;
+import io.vertx.ext.mail.*;
 
 /**
  * code chunks for the adoc documentation
@@ -78,6 +75,16 @@ public class MailExamples {
     attachment.setContentId("<image1@example.com>");
 
     message.setInlineAttachment(attachment);
+  }
+
+  public void createDKIMMailClient(Vertx vertx) {
+    DKIMSignOptions dkimSignOptions = new DKIMSignOptions();
+    dkimSignOptions.setPubSecKeyOptions(new PubSecKeyOptions().setSecretKey("PKCS8 Private Key Base64 String"));
+    dkimSignOptions.setAuid("identifier@example.com");
+    dkimSignOptions.setSelector("selector");
+    dkimSignOptions.setSdid("example.com");
+    MailConfig config = new MailConfig().setDkimSignOption(dkimSignOptions).setEnableDKIM(true);
+    MailClient mailClient = MailClient.createShared(vertx, config);
   }
 
   public void sendMail(MailMessage message, MailClient mailClient) {
